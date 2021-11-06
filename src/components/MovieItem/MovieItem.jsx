fa9b044149a878b453c4b786ef2a1c29e37e184c
Isiah Smith <isiahthomassmith@gmail.com>
2021-11-06 13:53:56 -0500

    MovieItem set to fetch details

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,13 +1,23 @@
+import {useHistory} from 'react-router-dom';
+import {useDispatch} from 'react-redux';
 
 function MovieItem({ movie }) {
+    const history = useHistory();
+    const dispatch = useDispatch();
 
-
+    const goToDetails = () => {
+        dispatch({type: 'FETCH_DETAILS', payload: movie})
+        // history.push('/details');
+    }
 
     return (
+        <>
         <div>
             <h3>{movie.title}</h3>
             <img src={movie.poster} alt={movie.title} />
         </div>
+        <button onClick={goToDetails}>Details</button>
+        </>
     )
 }
 
